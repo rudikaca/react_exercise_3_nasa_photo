37e3726aa2fa7c0a4ad0ad3968fae8006af4d113
rudinkaca <rudin.kaca@crispybacon.al>
2022-11-16 17:52:45 +0100

    fix: fix request

--- src/App.js
+++ src/App.js
@@ -7,21 +7,30 @@ import axios from "axios";
 function App() {
     const [date, setDate] = useState('2012-03-14');
     const [data, setData] = useState({});
+    const [error, setError] = useState();
+
     // const [data] = useState(dummyData);
 
     useEffect(() => {
-        axios.get(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=${date}`).then((response) => {
-            setData(response.data);
-            console.log(data);
-        })
-        // console.log(data);
+        async function fetchData() {
+            axios.get(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=${date}`).then((response) => {
+                if (response.data) {
+                    setData(response.data);
+                    // console.log(data);
+                }
+            })
+                .catch((e) => {
+                    setError(e.response.data.error.message)
+                })
+        }
+        fetchData();
     }, [date]);
 
     if (!data) return <h2>Loading...</h2>;
 
   return (
     <div className="App">
-        <Card data={data} date={date} setDate={setDate} />
+        {data.length ? <Card data={data} date={date} setDate={setDate} /> : <h2 style={{color: 'red'}}>{error}</h2>}
     </div>
   );
 }
